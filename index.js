336c2c149932b8e366e43bbaaf6812ae87757171
andymedina1 <107598069+andymedina1@users.noreply.github.com>
2024-08-22 19:17:51 -0300

    Finish Challenges

--- index.js
+++ index.js
@@ -1,20 +1,26 @@
-var express = require('express');
-var cors = require('cors');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config()
 
-var app = express();
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+const app = express()
+
+app.use(cors())
+app.use('/public', express.static(process.cwd() + '/public'))
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  const { originalname, mimetype, size } = req.file
+
+  res.json({ name: originalname, type: mimetype, size })
+})
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+  res.sendFile(process.cwd() + '/views/index.html')
+})
 
-
-
-
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log('Your app is listening on port ' + port)
-});
+})
